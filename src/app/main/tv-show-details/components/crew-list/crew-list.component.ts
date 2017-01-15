72c892611c2eb928fb28732208109e172ef1084f
CirXe0N <dw1ls@outlook.com>
2017-01-15 23:18:15 +0100

    Add cast & crew list to episode details component

--- src/app/main/tv-show-details/components/crew-list/crew-list.component.ts
+++ src/app/main/tv-show-details/components/crew-list/crew-list.component.ts
@@ -2,7 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'crew-list',
-  templateUrl: './crew-list.component.html'
+  templateUrl: './crew-list.component.html',
+  styleUrls: ['./crew-list.component.sass']
 })
 
 export class CrewListComponent {}
