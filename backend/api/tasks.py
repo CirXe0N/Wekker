5932cda76c23d563a8643f19473d478e6e5a8420
CirXe0N <dw1ls@outlook.com>
2017-02-21 18:21:32 +0100

    Add tasks to keep tv show and movie information up to date

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -2,6 +2,8 @@ from celery.schedules import crontab
 from celery.task import periodic_task
 from celery.task import task
 from celery.utils.log import get_task_logger
+from django.core.management import call_command
+
 from api.utilities import emails, reminders
 
 logger = get_task_logger(__name__)
@@ -32,4 +34,18 @@ def send_tv_show_episode_reminder_task(email_address, episode_id):
 def send_movie_reminder_task(email_address, movie_id):
     logger.info('Starting')
     emails.send_movie_reminder_email(email_address, movie_id)
+    logger.info('Finished')
+
+
+@periodic_task(run_every=crontab(minute=0, hour=6), name="update_tv_shows")
+def update_tv_shows():
+    logger.info('Starting')
+    call_command('updatetvshows')
+    logger.info('Finished')
+
+
+@periodic_task(run_every=crontab(minute=0, hour=3), name="update_movies")
+def update_movies():
+    logger.info('Starting')
+    call_command('updatemovies')
     logger.info('Finished')
